9ef102f6b61e0f98ca066a75b27196789de7d05a
AndreiSva <andreisva2023@gmail.com>
2024-01-05 17:29:52 -0800

    fix journal sorting

--- src/lib/journals.js
+++ src/lib/journals.js
@@ -6,7 +6,16 @@ export function get_journals(n = 0) {
     const journal_files = fs.readdirSync(journal_dir);
 
     journal_files.sort((a, b) => {
-        a.localeCompare(b);
+        // Remove the .md extension and split the date by "-"
+        let dateA = a.slice(0, -3).split("-");
+        let dateB = b.slice(0, -3).split("-");
+
+        // Create new Date objects
+        let dateObjA = new Date(dateA[2], dateA[1] - 1, dateA[0]);
+        let dateObjB = new Date(dateB[2], dateB[1] - 1, dateB[0]);
+
+        // Compare the Date objects
+        return dateObjA - dateObjB;
     });
     journal_files.reverse();
 
