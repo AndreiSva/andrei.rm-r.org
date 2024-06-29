b60e1f383924f27c7e39d90b120478bdcc3d048c
Andrei <andreisva2023@gmail.com>
2024-06-29 00:14:44 -0700

    deploy: recursively rsync
    I forgot this at first

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 npm run build
-rsync -a --progress ./build andrei@rm-r.org:/var/www/andrei.rm-r.org/
-rsync -a --progress ./static andrei@rm-r.org:/var/www/andrei.rm-r.org/
+rsync -ar --progress ./build andrei@rm-r.org:/var/www/andrei.rm-r.org/
+rsync -ar --progress ./static andrei@rm-r.org:/var/www/andrei.rm-r.org/
 systemctl --host=root@rm-r.org restart AndreiSite
